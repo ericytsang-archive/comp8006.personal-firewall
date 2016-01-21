6efeab6fc3f3bb8a3841992b8cbb0b0e0340d7da
Eric Tsang <surplus.et@gmail.com>
2016-01-21 13:31:37 -0800

    consolidated rules for www traffic
    Signed-off-by: Eric Tsang <surplus.et@gmail.com>

--- firewall.sh
+++ firewall.sh
@@ -10,6 +10,11 @@ iptables -P INPUT ACCEPT
 iptables -P OUTPUT ACCEPT
 iptables -P FORWARD ACCEPT
 
+echo "# set chain policies"
+iptables -P INPUT DROP
+iptables -P OUTPUT DROP
+iptables -P FORWARD DROP
+
 echo "# WWW chain"
 iptables -N WWW
 iptables -A WWW -j ACCEPT
@@ -18,43 +23,26 @@ echo "# SSH chain"
 iptables -N SSH
 iptables -A SSH -j ACCEPT
 
-# todo: test to see which rules get packets, and try to consolidate the rules
+echo "# configure chains to accept WWW traffic"
 
 echo "# INPUT chain"
-iptables -A INPUT -i any -p icmp -j ACCEPT
-iptables -A INPUT -i lo  -p tcp -m multiport --dport 80  -j WWW
-iptables -A INPUT -i lo  -p udp -m multiport --dport 80  -j WWW
-iptables -A INPUT -i lo  -p tcp -m multiport --dport 443 -j WWW
-iptables -A INPUT -i lo  -p udp -m multiport --dport 443 -j WWW
-iptables -A INPUT -i lo  -p tcp -m multiport --dport 53  -j WWW
-iptables -A INPUT -i lo  -p udp -m multiport --dport 53  -j WWW
-iptables -A INPUT -i lo  -j ACCEPT
-iptables -A INPUT -i any -p tcp -m multiport --sport 80  -j WWW
-iptables -A INPUT -i any -p udp -m multiport --sport 80  -j WWW
-iptables -A INPUT -i any -p tcp -m multiport --sport 443 -j WWW
-iptables -A INPUT -i any -p udp -m multiport --sport 443 -j WWW
-iptables -A INPUT -i any -p udp -m multiport --sport 53  -j WWW
-iptables -A INPUT -j ACCEPT
-iptables -P INPUT DROP
-
-# todo: test to see which rules get packets, and try to consolidate the rules
+iptables -A INPUT -i lo -p udp -m multiport --dport 53 -j WWW
+iptables -A INPUT -p tcp -m multiport --sport 80,443 -j WWW
+iptables -A INPUT -p udp -m multiport --sport 53 -j WWW
+iptables -A INPUT -p icmp -j WWW
 
 echo "# OUTPUT chain"
-iptables -A OUTPUT -o any -p icmp -j ACCEPT
-iptables -A OUTPUT -o lo  -p tcp -m multiport --sport 80  -j WWW
-iptables -A OUTPUT -o lo  -p udp -m multiport --sport 80  -j WWW
-iptables -A OUTPUT -o lo  -p tcp -m multiport --sport 443 -j WWW
-iptables -A OUTPUT -o lo  -p udp -m multiport --sport 443 -j WWW
-iptables -A OUTPUT -o lo  -p tcp -m multiport --sport 53  -j WWW
-iptables -A OUTPUT -o lo  -p udp -m multiport --sport 53  -j WWW
-iptables -A OUTPUT -o lo  -j ACCEPT
-iptables -A OUTPUT -o any -p tcp -m multiport --dport 80  -j WWW
-iptables -A OUTPUT -o any -p udp -m multiport --dport 80  -j WWW
-iptables -A OUTPUT -o any -p tcp -m multiport --dport 443 -j WWW
-iptables -A OUTPUT -o any -p udp -m multiport --dport 443 -j WWW
-iptables -A OUTPUT -o any -p udp -m multiport --dport 53  -j WWW
-iptables -A INPUT -j ACCEPT
-iptables -P OUTPUT DROP
+iptables -A OUTPUT -o lo -p udp -m multiport --sport 53 -j WWW
+iptables -A OUTPUT -p tcp -m multiport --dport 80,443 -j WWW
+iptables -A OUTPUT -p udp -m multiport --dport 53 -j WWW
+iptables -A OUTPUT -p icmp -j WWW
 
-echo "# FORWARD chain"
-iptables -P FORWARD DROP
+echo "# configure chains to accept SSH traffic"
+
+echo "# INPUT chain"
+iptables -A INPUT -p tcp -m multiport --sport 22 -j SSH
+iptables -A INPUT -p udp -m multiport --sport 22 -j SSH
+
+echo "# OUTPUT chain"
+iptables -A OUTPUT -p tcp -m multiport --dport 22 -j SSH
+iptables -A OUTPUT -p udp -m multiport --dport 22 -j SSH
