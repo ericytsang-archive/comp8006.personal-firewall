e78506bf2207adf179a670bdf84119b2d81f4a0c
Eric Tsang <surplus.et@gmail.com>
2016-01-21 06:29:23 -0800

    idkwhatsgoingon
    Signed-off-by: Eric Tsang <surplus.et@gmail.com>

--- firewall.sh
+++ firewall.sh
@@ -19,12 +19,15 @@ iptables -N SSH
 iptables -A SSH -j ACCEPT
 
 echo "# INPUT chain"
-iptables -A INPUT -i lo -j ACCEPT
-iptables -A INPUT -p icmp -j ACCEPT
-iptables -A INPUT -p tcp -m multiport --sport 80,443 -j WWW
-iptables -A INPUT -p udp -m multiport --sport 80,443,53 -j WWW
-iptables -A INPUT -p tcp -m multiport --sport 22 -j SSH # test
-iptables -A INPUT -p udp -m multiport --sport 22 -j SSH # test
+# WWW traffic
+iptables -A INPUT -i any -p icmp -j ACCEPT
+iptables -A INPUT -i any -p tcp -m multiport --sport 80,443 -j WWW
+iptables -A INPUT -i any -p udp -m multiport --sport 80,443,53 -j WWW
+# iptables -A INPUT -i lo -j WWW
+iptables -A INPUT -i lo  -p udp -m multiport --dport 53 -j WWW
+# SSH traffic
+# iptables -A INPUT -i any -p tcp -m multiport --sport 22 -j SSH # test
+# iptables -A INPUT -i any -p udp -m multiport --sport 22 -j SSH # test
 iptables -P INPUT DROP
 
 echo "# OUTPUT chain"
@@ -32,8 +35,8 @@ iptables -A OUTPUT -o lo -j ACCEPT
 iptables -A OUTPUT -p icmp -j ACCEPT
 iptables -A OUTPUT -p tcp -m multiport --dport 80,443 -j WWW
 iptables -A OUTPUT -p udp -m multiport --dport 80,443,53 -j WWW
-iptables -A OUTPUT -p tcp -m multiport --dport 22 -j SSH # test
-iptables -A OUTPUT -p udp -m multiport --dport 22 -j SSH # test
+# iptables -A OUTPUT -p tcp -m multiport --dport 22 -j SSH # test
+# iptables -A OUTPUT -p udp -m multiport --dport 22 -j SSH # test
 iptables -P OUTPUT DROP
 
 echo "# FORWARD chain"
