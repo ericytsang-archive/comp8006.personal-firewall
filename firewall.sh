f691e298686baca5f6146f4cb1be3221e654ec7c
Eric Tsang <surplus.et@gmail.com>
2016-01-21 06:29:23 -0800

    created many simple rules. see which ones are used.
    * remove the rules that aren't needed, and consolidate the rest
    
    Signed-off-by: Eric Tsang <surplus.et@gmail.com>

--- firewall.sh
+++ firewall.sh
@@ -18,22 +18,42 @@ echo "# SSH chain"
 iptables -N SSH
 iptables -A SSH -j ACCEPT
 
+# todo: test to see which rules get packets, and try to consolidate the rules
+
 echo "# INPUT chain"
-iptables -A INPUT -i lo -j ACCEPT
-iptables -A INPUT -p icmp -j ACCEPT
-iptables -A INPUT -p tcp -m multiport --sport 80,443 -j WWW
-iptables -A INPUT -p udp -m multiport --sport 80,443,53 -j WWW
-iptables -A INPUT -p tcp -m multiport --sport 22 -j SSH # test
-iptables -A INPUT -p udp -m multiport --sport 22 -j SSH # test
+iptables -A INPUT -i any -p icmp -j ACCEPT
+iptables -A INPUT -i lo  -p tcp -m multiport --dport 80  -j WWW
+iptables -A INPUT -i lo  -p udp -m multiport --dport 80  -j WWW
+iptables -A INPUT -i lo  -p tcp -m multiport --dport 443 -j WWW
+iptables -A INPUT -i lo  -p udp -m multiport --dport 443 -j WWW
+iptables -A INPUT -i lo  -p tcp -m multiport --dport 53  -j WWW
+iptables -A INPUT -i lo  -p udp -m multiport --dport 53  -j WWW
+iptables -A INPUT -i lo  -j ACCEPT
+iptables -A INPUT -i any -p tcp -m multiport --sport 80  -j WWW
+iptables -A INPUT -i any -p udp -m multiport --sport 80  -j WWW
+iptables -A INPUT -i any -p tcp -m multiport --sport 443 -j WWW
+iptables -A INPUT -i any -p udp -m multiport --sport 443 -j WWW
+iptables -A INPUT -i any -p udp -m multiport --sport 53  -j WWW
+iptables -A INPUT -j ACCEPT
 iptables -P INPUT DROP
 
+# todo: test to see which rules get packets, and try to consolidate the rules
+
 echo "# OUTPUT chain"
-iptables -A OUTPUT -o lo -j ACCEPT
-iptables -A OUTPUT -p icmp -j ACCEPT
-iptables -A OUTPUT -p tcp -m multiport --dport 80,443 -j WWW
-iptables -A OUTPUT -p udp -m multiport --dport 80,443,53 -j WWW
-iptables -A OUTPUT -p tcp -m multiport --dport 22 -j SSH # test
-iptables -A OUTPUT -p udp -m multiport --dport 22 -j SSH # test
+iptables -A OUTPUT -o any -p icmp -j ACCEPT
+iptables -A OUTPUT -o lo  -p tcp -m multiport --sport 80  -j WWW
+iptables -A OUTPUT -o lo  -p udp -m multiport --sport 80  -j WWW
+iptables -A OUTPUT -o lo  -p tcp -m multiport --sport 443 -j WWW
+iptables -A OUTPUT -o lo  -p udp -m multiport --sport 443 -j WWW
+iptables -A OUTPUT -o lo  -p tcp -m multiport --sport 53  -j WWW
+iptables -A OUTPUT -o lo  -p udp -m multiport --sport 53  -j WWW
+iptables -A OUTPUT -o lo  -j ACCEPT
+iptables -A OUTPUT -o any -p tcp -m multiport --dport 80  -j WWW
+iptables -A OUTPUT -o any -p udp -m multiport --dport 80  -j WWW
+iptables -A OUTPUT -o any -p tcp -m multiport --dport 443 -j WWW
+iptables -A OUTPUT -o any -p udp -m multiport --dport 443 -j WWW
+iptables -A OUTPUT -o any -p udp -m multiport --dport 53  -j WWW
+iptables -A INPUT -j ACCEPT
 iptables -P OUTPUT DROP
 
 echo "# FORWARD chain"
