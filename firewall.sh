05405ee1a017cb9b1d6b3a954d8ab5db7987a895
Eric Tsang <surplus.et@gmail.com>
2016-01-21 00:51:11 -0800

    first commit

---
+++ firewall.sh
@@ -0,0 +1,40 @@
+
+echo "### configuration below ###"
+
+echo "### code below - do not touch! ###"
+
+echo "# reset firewall"
+iptables -F
+iptables -X
+iptables -P INPUT ACCEPT
+iptables -P OUTPUT ACCEPT
+iptables -P FORWARD ACCEPT
+
+echo "# WWW chain"
+iptables -N WWW
+iptables -A WWW -j ACCEPT
+
+echo "# SSH chain"
+iptables -N SSH
+iptables -A SSH -j ACCEPT
+
+echo "# INPUT chain"
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -p icmp -j ACCEPT
+iptables -A INPUT -p tcp -m multiport --sport 80,443 -j WWW
+iptables -A INPUT -p udp -m multiport --sport 80,443,53 -j WWW
+iptables -A INPUT -p tcp -m multiport --sport 22 -j SSH # test
+iptables -A INPUT -p udp -m multiport --sport 22 -j SSH # test
+iptables -P INPUT DROP
+
+echo "# OUTPUT chain"
+iptables -A OUTPUT -o lo -j ACCEPT
+iptables -A OUTPUT -p icmp -j ACCEPT
+iptables -A OUTPUT -p tcp -m multiport --dport 80,443 -j WWW
+iptables -A OUTPUT -p udp -m multiport --dport 80,443,53 -j WWW
+iptables -A OUTPUT -p tcp -m multiport --dport 22 -j SSH # test
+iptables -A OUTPUT -p udp -m multiport --dport 22 -j SSH # test
+iptables -P OUTPUT DROP
+
+echo "# FORWARD chain"
+iptables -P FORWARD DROP
