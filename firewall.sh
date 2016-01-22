6563af1f5e6a110798fffc157206f111003ce28f
Eric Tsang <surplus.et@gmail.com>
2016-01-22 08:12:42 -0800

    separated icmp rules from www rules
    Signed-off-by: Eric Tsang <surplus.et@gmail.com>

--- firewall.sh
+++ firewall.sh
@@ -53,17 +53,17 @@ echo "# enable DHCP"
 iptables -A INPUT -p udp -m multiport --dport 67,68 -j DHCP # test
 iptables -A OUTPUT -p udp -m multiport --sport 67,68 -j DHCP # test
 
+echo "# enable ICMP"
+iptables -A INPUT -p icmp -j WWW_CLNT
+iptables -A OUTPUT -p icmp -j WWW_CLNT
+
 echo "# enable web hosting"
 iptables -A INPUT -p tcp -m multiport --dport 80,443 -m multiport --sport 1024:65535 -j WWW_SVR
 iptables -A OUTPUT -p tcp -m multiport --sport 80,443 -m multiport --dport 1024:65535 -j WWW_SVR
 
 echo "# enable web browsing"
-
 iptables -A INPUT -p tcp -m multiport --sport 80,443 -j WWW_CLNT
-iptables -A INPUT -p icmp -j WWW_CLNT
-
 iptables -A OUTPUT -p tcp -m multiport --dport 80,443 -j WWW_CLNT
-iptables -A OUTPUT -p icmp -j WWW_CLNT
 
 echo "# enable connections to local SSH server"
 iptables -A INPUT  -p tcp -m multiport --dport $LOCAL_SSH_SERVERS -m multiport --sport $INBOUND_SSH_CLIENTS --tcp-flags NONE NONE -j SSH # test
